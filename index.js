8914f2a04901e584e45054bc0ccd57881b9b288d
Valo Reyes <conspiringhumanity@gmail.com>
2018-08-12 19:34:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays() {
   return 'Happy holidays!'
 }
 function happyHolidaysTo(name) {
-  return "Happy holiday, you!"
+  return "Happy holidays, you!"
 }
 function happyHolidaysTo(holiday, name) {
   return 'Happy holiday, you!'
